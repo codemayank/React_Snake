8a8d9efd63cf5e425f0f9bd8a214ac288d6f756f
Mayank <opthami@gmail.com>
2019-01-20 11:45:25 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -10,29 +10,38 @@ const Food = () => {
   return <div className="segment food" />
 }
 
-const initialGameState = function() {
+const Snake = () => {
+  return <div className="segment snake" />
+}
+
+const Wall = () => {
+  return <div className="segment wall" />
+}
+
+const initialGameState = function(startPoint, snakeWidth, velocity) {
   return {
-    snakeDivs: [this.generateFood()],
-    foodPosition: 48,
+    snakeDivs: [startPoint],
+    foodPosition: this.createSegment(),
     moveDirection: {
       direction: 'Horizontal',
       increment: 1
     },
-    currentSnakeWidth: 20,
+    currentSnakeWidth: snakeWidth,
     gameOver: false,
-    velocity: 200
+    velocity: velocity
   }
 }
 
 class PlayArea extends React.Component {
   constructor(props) {
     super(props)
-    this.state = initialGameState.call(this)
-    this.gridSize = 21
+    this.state = initialGameState.call(this, 250, 20, 200)
+    this.gridSize = 20
     this.handleKeyPress = this.handleKeyPress.bind(this)
     this.updateSnake = this.updateSnake.bind(this)
     this.tick = this.tick.bind(this)
     this.startGame = this.startGame.bind(this)
+    this.createFood = this.createFood.bind(this)
   }
 
   componentWillUnmount() {
@@ -41,7 +50,7 @@ class PlayArea extends React.Component {
 
   startGame = () => {
     if (this.state.gameOver) {
-      this.setState(initialGameState.call(this))
+      this.setState(initialGameState.call(this, 250, 20, 200))
     }
     this.tick()
   }
@@ -63,8 +72,8 @@ class PlayArea extends React.Component {
       } else if (
         this.state.snakeDivs[0] < 0 ||
         this.state.snakeDivs[0] >= 400 ||
-        this.state.snakeDivs[0] % 20 === 19 ||
-        this.state.snakeDivs[0] % 20 === 0
+        this.state.snakeDivs[0] % 20 === 0 ||
+        this.state.snakeDivs[0] % 20 === 19
       ) {
         this.endGame()
       } else {
@@ -73,7 +82,6 @@ class PlayArea extends React.Component {
             if (idx === 0) {
               return position + this.state.moveDirection.increment
             } else {
-              console.log(prevState.snakeDivs[idx - 1])
               return prevState.snakeDivs[idx - 1]
             }
           })
@@ -137,35 +145,48 @@ class PlayArea extends React.Component {
   updateSnake = () => {
     this.setState(prevState => ({
       snakeDivs: prevState.snakeDivs.concat([prevState.snakeDivs[0] + 1]),
-      foodPosition: this.generateFood()
+      foodPosition: this.createFood()
     }))
   }
 
-  generateFood = () => {
+  createSegment = () => {
     const min = 20
-    const max = 380
+    const max = 400
     return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  createFood = () => {
+    let foodPosition = this.createSegment()
+    //food should not be created on snake body or on the walls
+    while (
+      this.state.snakeDivs.indexOf(foodPosition) !== -1 ||
+      foodPosition % 20 === 0 ||
+      foodPosition % 20 === 19
+    ) {
+      foodPosition = this.createSegment()
+    }
+    return foodPosition
   }
 
   render() {
     const grid = []
-    let snakeStyle = {
-      backgroundColor: 'blue',
-      width: `${this.state.currentSnakeWidth}px`
-    }
-    for (let i = 0; i < this.gridSize * this.gridSize - 1; i++) {
+    for (let i = 0; i < this.gridSize * this.gridSize; i++) {
       if (this.state.snakeDivs.indexOf(i) !== -1) {
-        grid.push(<div className="segment snake" key={i} style={snakeStyle} />)
+        grid.push(<Snake key={i} />)
       } else if (i === this.state.foodPosition) {
         grid.push(<Food key={i} />)
+      } else if (i % 20 === 19 || i % 20 === 0) {
+        grid.push(<Wall />)
       } else {
         grid.push(<GridItem key={i} />)
       }
     }
     return (
       <div onKeyDown={this.handleKeyPress} tabIndex="0">
-        <div className="play-info">
+        <div className="player info">
           <h2>score: {this.state.snakeDivs.length - 1}</h2>
+        </div>
+        <div className="player controls">
           <button onClick={this.startGame}>Start</button>
           <button onClick={this.endGame}>End Game</button>
         </div>
@@ -176,12 +197,6 @@ class PlayArea extends React.Component {
         </div>
       </div>
     )
-  }
-}
-
-class Grid extends React.Component {
-  constructor(props) {
-    super(props)
   }
 }
 
