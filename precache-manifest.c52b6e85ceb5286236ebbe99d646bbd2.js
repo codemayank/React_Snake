1d6f84796cfe84fcf45b9cbc61785d2e387a64a7
Mayank <opthami@gmail.com>
2019-01-20 12:13:36 +0530

    Updates

---
+++ precache-manifest.c52b6e85ceb5286236ebbe99d646bbd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "313a7b21a809375905a2",
+    "url": "/React_Snake/static/js/runtime~main.313a7b21.js"
+  },
+  {
+    "revision": "fa7efdf6492aa2c345f7",
+    "url": "/React_Snake/static/js/main.fa7efdf6.chunk.js"
+  },
+  {
+    "revision": "d0d9aea5003583dfc7b5",
+    "url": "/React_Snake/static/js/1.d0d9aea5.chunk.js"
+  },
+  {
+    "revision": "fa7efdf6492aa2c345f7",
+    "url": "/React_Snake/static/css/main.38f9e8e6.chunk.css"
+  },
+  {
+    "revision": "69b5a43ebb515994fed07ec90d9ee846",
+    "url": "/React_Snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/snake/precache-manifest.d3697351fa59d711a660719054265599.js"
+  "/React_Snake/precache-manifest.c52b6e85ceb5286236ebbe99d646bbd2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/snake/index.html", {
+workbox.routing.registerNavigationRoute("/React_Snake/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
